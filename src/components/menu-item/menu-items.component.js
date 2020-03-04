9b9cf7fcb5aa36bf818282c702e3f9ffbe425e4f
Felipe Cavalcanti <fel.cavalcanti@hotmail.com>
2020-03-04 19:25:01 -0300

    add style changes in menu-item component

--- src/components/menu-item/menu-items.component.js
+++ src/components/menu-item/menu-items.component.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './menu-items.style.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div 
-        style={{backgroundImage: `url(${imageUrl})`}} 
-        className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div  style={{backgroundImage: `url(${imageUrl})`}} 
+        className="menu-item__background-image"></div>
         <div className="menu-item__content">
-            <h1 className="menu-item__content--title"> {title}</h1>
+            <h1 className="menu-item__content--title"> {title.toUpperCase()}</h1>
             <h2 className="menu-item__content--subtitle"> SHOP NOW</h2>
         </div>
     </div>
