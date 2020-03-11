d65cb893ae69bea129d9a8ea79b7ca836f9bed7e
Felipe Cavalcanti <fel.cavalcanti@hotmail.com>
2020-03-11 19:59:53 -0300

    added sign-in sign-out page, added form-input, custom-button but only for the submit functionallity

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component.js";
 import Header from "./components/header/header.component.js";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/signIn" component={SignInSignUpPage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import SignIn from '../../components/sign-in/sign-in.component.js'
+
+const SignInSignUpPage = () => (
+    <div className='sign-in-and-sign-up'> 
+        <SignIn></SignIn>
+    </div>
+)
+
+export default SignInSignUpPage;
