b897ec8bd157919ad17531f44925b6e73df1c88e
Felipe Cavalcanti <31782119+Feellipe@users.noreply.github.com>
2020-03-15 14:36:43 -0300

    something just to test

--- src/App.js
+++ src/App.js
@@ -6,25 +6,24 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component.js";
 import Header from "./components/header/header.component.js";
-import {auth } from './firebase/firebase.utils'
-
+import { auth } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
-  componentDidMount(){
+  componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+      this.setState({ currentUser: user });
       console.log(user);
-    })
+    });
   }
 
   componentWillUnmount() {
@@ -34,7 +33,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/signIn" component={SignInSignUpPage} />
