165125a65a095db4e6ce24de12a889241298c86c
Felipe Cavalcanti <31782119+Feellipe@users.noreply.github.com>
2020-04-10 11:35:58 -0300

    Added WithSpinner Hoc, removed hard coded data

--- src/App.js
+++ src/App.js
@@ -5,41 +5,40 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component.js";
-import CheckoutPage from './pages/checkout/checkout.component.js'
+import CheckoutPage from "./pages/checkout/checkout.component.js";
 
 import Header from "./components/header/header.component.js";
-import { auth, createUserProfileDoc, addCollectionAndDocuments } from "./firebase/firebase.utils";
-import {connect} from 'react-redux'
-import {createStructuredSelector} from 'reselect';
-import { setCurrentUser } from './redux/user/user.actions'
-import {selectCurrentUser} from './redux/user/user.selector'
-import {selectCollectionsForPreview} from './redux/shop/shop.selectors'
+import { auth, createUserProfileDoc } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { selectCollectionsForPreview } from "./redux/shop/shop.selectors";
 
 class App extends React.Component {
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser, collectionsArray} = this.props
+    const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
+      if (userAuth) {
         const userRef = await createUserProfileDoc(userAuth);
         //the userRef is the return of the user we just created in the firestore
 
         // a documentSnapshot is the return of a documentRef
-        //this is setting the user after it got the Doc of the user in the firestore 
+        //this is setting the user after it got the Doc of the user in the firestore
         // the listener onSnapshot is listening for any change in the userRef
         userRef.onSnapshot(snapShot => {
-          setCurrentUser({ 
+          setCurrentUser({
             id: snapShot.id,
             ...snapShot.data()
-         }); 
+          });
         });
       }
 
       //esse está setando o user que foi autenticado pelo firebase
-      setCurrentUser( userAuth);      
+      setCurrentUser(userAuth);
     });
   }
 
@@ -48,7 +47,7 @@ class App extends React.Component {
   }
 
   render() {
-    const {currentUser} = this.props;
+    const { currentUser } = this.props;
 
     return (
       <div>
@@ -57,22 +56,30 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/checkout" component={CheckoutPage} />
-          <Route exact path="/signin" render={() => currentUser ? (<Redirect to='/' />) :  (<SignInSignUpPage/>)} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInSignUpPage />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   collectionsArray: selectCollectionsForPreview
-})
+});
 
 const mapDispatchToProps = dispatch => ({
-// passamos a função quq seta o state no reducer com o valor do objeto que queremos alterar, nesse caso user é o objeto no rootReducer que é setado no componentDid Mount
+  // passamos a função quq seta o state no reducer com o valor do objeto que queremos alterar, nesse caso user é o objeto no rootReducer que é setado no componentDid Mount
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,45 +1,69 @@
 import React from "react";
-import {Route} from 'react-router-dom'
-import {connect} from  'react-redux';
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {firestore, convertColletionsSnapshotToMap} from '../../firebase/firebase.utils'
+import {
+  firestore,
+  convertColletionsSnapshotToMap
+} from "../../firebase/firebase.utils";
 
-import {updateCollections} from '../../redux/shop/shop.actions'
+import { updateCollections } from "../../redux/shop/shop.actions";
 
+import WithSpinner from "../../components/with-spinner/with-spinner.components";
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from '../collection/collection.component'
+import CollectionPage from "../collection/collection.component";
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
 
   unsubscribeFromSnapshot = null;
 
-  componentDidMount(){
-    const {updateCollections} = this.props
+  componentDidMount() {
+    const { updateCollections } = this.props;
 
-    const collectionRef = firestore.collection('collections');
+    const collectionRef = firestore.collection("collections");
 
-
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot( async snapshot => {
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertColletionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap)
-    })
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
-    render(){
-      const {match} = this.props;
-      return (
-        <div className="shop-page">
-          <Route exact path={`${match.path}`} component={CollectionOverview} />
-          <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> 
-        </div>
-      );
-    }
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
   updateCollections: collectionsMap =>
-    dispatch(updateCollections(collectionsMap))  
-})
+    dispatch(updateCollections(collectionsMap))
+});
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
