8027bcafb884e333619ee7777b313fdf61959bca
Felipe Cavalcanti <fel.cavalcanti@hotmail.com>
2020-11-12 16:42:38 -0300

    addded lean

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/lean-caviar-preta.svg";
 import {auth} from '../../firebase/firebase.utils'
 import {connect} from 'react-redux'
 import {createStructuredSelector} from 'reselect';
